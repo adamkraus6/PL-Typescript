5e9a83cbec8225f83e39d914437eaf78ebca2659
Adam Kraus <adamkraus6@gmail.com>
2022-11-21 13:40:01 -0700

    some TODOs

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,32 @@ import { AppDataSource } from "./data-source";
 // import { User } from "./entity/User"
 import * as express from "express";
 import * as path from "path";
-import * as bodyparser from "body-parser"
+import * as bodyparser from "body-parser";
 
-const PORT: number = 3000
+const PORT: number = 3000;
 
 AppDataSource.initialize()
   .then(async () => {
     const app = express();
 
-	app.use(express.static("src"), bodyparser.json(), bodyparser.urlencoded({extended: false}))
+    app.use(
+      express.static("src"),
+      bodyparser.json(),
+      bodyparser.urlencoded({ extended: false })
+    );
 
     app.get("/", (req, res) => {
       res.sendFile(path.join(__dirname, "/html/index.html"));
     });
 
+    app.get("/add", (req, res) => {
+      res.sendFile(path.join(__dirname, "/html/add.html"));
+    });
+
+	// TODO: post for new actor, new movie, new role
+
     app.listen(PORT, () => {
       console.log(`Listening on port ${PORT}`);
     });
-
-	app.post("/test", (req, res) => {
-		console.log(req.body)
-		res.redirect("back")
-	})
   })
   .catch((error) => console.log(error));
